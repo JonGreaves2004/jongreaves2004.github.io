ee1951dd494b60fe77c5d69a3f8a954126aed607
JonGreaves2004 <163329476+JonGreaves2004@users.noreply.github.com>
2025-07-25 11:43:00 +0100

    Update household_lob_lists.js

--- household_lob_lists.js
+++ household_lob_lists.js
@@ -651,7 +651,14 @@ $(document).ready(function () {
 
 
 
-
+    $('#type_of_heating').on('change', function() {
+    const selectedValue = $(this).val();
+    if (['Gas Fired Central Heating'].includes(selectedValue)) {
+      $('#boiler_types').css('display','block');
+    } else {
+      $('#boiler_types').css('display','none'); 
+    }
+  });
   
 
   
